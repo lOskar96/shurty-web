5592ec3795eb6f1aedfbb7a4e816e14643cd2287
lOskar <llOskaR96@gmail.com>
2025-12-02 17:56:41 +0100

    feat: Initialize new React project with Vite.

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    sourcemap: true,
+  },
+});
